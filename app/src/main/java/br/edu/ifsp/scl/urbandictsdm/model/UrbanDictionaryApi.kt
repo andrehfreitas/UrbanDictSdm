bae90b02b2af57e201374a03cf8d4b558cb1d336
andrehfreitas <andrehfreitas@gmail.com>
2020-04-08 21:55:31 -0300

    Commit do projeto completo

---
+++ app/src/main/java/br/edu/ifsp/scl/urbandictsdm/model/UrbanDictionaryApi.kt
@@ -0,0 +1,10 @@
+package br.edu.ifsp.scl.urbandictsdm.model
+
+object UrbanDictionaryApi {
+    val URL_BASE = "https://mashape-community-urban-dictionary.p.rapidapi.com/"
+    val END_POINT = "define?term=" // Não é bem um endpoint, mas...
+    val X_RAPIDAPI_KEY_FIELD = "x-rapidapi-key"
+    val X_RAPIDAPI_KEY_VALUE = "24acc9340amsh94046be68806a5bp1aae5ajsn21e70acea3a2"
+    val X_RAPIDAPI_HOST_FIELD = "x-rapidapi-host"
+    val X_RAPIDAPI_HOST_VALUE = "mashape-community-urban-dictionary.p.rapidapi.com"
+}
